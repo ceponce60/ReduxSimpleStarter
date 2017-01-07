dd2352874f8f4ffd1fd8f9984b1bfd2b264b0ef2
ceponce60 <ceponce60@gmail.com>
2017-01-07 12:11:22 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
-const API_KEY = 'AIzaSyBUgNLmB1txfwBYZcrtk6V5l4TXv1P-rgI';
+const API_KEY = '';
 
 class App extends React.Component {
     constructor(props) {
